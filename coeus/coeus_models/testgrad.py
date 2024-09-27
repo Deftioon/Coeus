4782a185f2a7fe1071c2c30be757ee34773f1815
Deftioon <jylu1205@outlook.com>
2024-09-27 10:52:14 +0800

    hhhh

--- coeus/coeus_models/testgrad.py
+++ coeus/coeus_models/testgrad.py
@@ -1,58 +1,14 @@
 import numpy as np
 import cupy as cp
 
-# class autograd:
-#     def __init__(self, data, parents=None, op=None):
-#         self.data = data
-#         self.grad = 0.0
-#         self.parents = parents or []
-#         self.op = op
-    
-#     def backward(self, grad=1.0):
-#         self.grad += grad
-#         for parent, local_grad in self.parents:
-#             parent.backward(grad * local_grad(self))
-
-#     def __add__(self, other):
-#         other = other if isinstance(other, autograd) else autograd(other)
-#         return autograd(
-#             self.data + other.data,
-#             parents=[(self, lambda _: 1), (other, lambda _: 1)], op='+')
-
-#     def __mul__(self, other):
-#         other = other if isinstance(other, autograd) else autograd(other)
-#         return autograd(
-#             self.data * other.data,
-#             parents=[
-#                 (self, lambda v: other.data),
-#                 (other, lambda v: self.data)],
-#             op='*')
-
-#     def __sub__(self, other):
-#         other = other if isinstance(other, autograd) else autograd(other)
-#         return autograd(
-#             self.data - other.data,
-#             parents=[(self, lambda _: 1), (other, lambda _: -1)], op='-')
-
-#     def __truediv__(self, other):
-#         other = other if isinstance(other, autograd) else autograd(other)
-#         return autograd(
-#             self.data / other.data,
-#             parents=[
-#                 (self, lambda v: 1/other.data),
-#                 (other, lambda v: -self.data/other.data**2)],
-#             op='/')
-
-#     def __repr__(self):
-#         return f"autograd(data={self.data}, grad={self.grad})"
-
 class autograd:
-    def __init__(self, data, parents=None, op=None, device = "cpu"):
+    def __init__(self, data, parents=None, op=None, device="cpu"):
         if device == "cpu":
             self.device = np
-        
-        if device == "gpu":
+        elif device == "gpu":
             self.device = cp
+        else:
+            raise ValueError("Unsupported device. Use 'cpu' or 'gpu'.")
 
         self.data = self.device.array(data)
         self.grad = self.device.zeros(self.data.shape)
@@ -71,8 +27,8 @@ class autograd:
         return autograd(
             self.data.transpose(),
             parents=[(self, lambda v: v.transpose())],
-            op='transpose')
-
+            op='transpose'
+        )
     
     def __add__(self, other):
         other = other if isinstance(other, autograd) else autograd(other)
@@ -94,9 +50,11 @@ class autograd:
         return autograd(
             self.data @ other.data,
             parents=[
-                (self, lambda v: other.data),
-                (other, lambda v: self.data)],
-            op='@')
+                (self, lambda v: other.data.T),
+                (other, lambda v: self.data.T)
+            ],
+            op='@'
+        )
     
     def __pow__(self, other):
         other = other if isinstance(other, autograd) else autograd(other)
@@ -106,7 +64,7 @@ class autograd:
                 (self, lambda v: other.data * self.data**(other.data-1)),
                 (other, lambda v: self.data**other.data * np.log(self.data))],
             op='**')
-
+    
     def __neg__(self):
         return autograd(-self.data, parents=[(self, lambda _: -1)], op='-')
     
@@ -124,16 +82,13 @@ class autograd:
                 (self, lambda v: 1/other.data),
                 (other, lambda v: -self.data/other.data**2)],
             op='/')
-    
-    
 
     def __repr__(self):
         return f"autograd(data={self.data}, grad={self.grad})"
-    
+
 if __name__ == "__main__":
-    x = autograd(np.array([[1,2,3],[4,5,6]]))
-    y = autograd(np.array([[7,8,9],[10,11,12]]))
-    z = x @ y.transpose()
+    x = autograd([1,2,3])
+    y = autograd([0.5,0.5,0.5])
+    z = x ** y
     z.backward()
     print(x.grad)
-    print(y.grad)
