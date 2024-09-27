1688efb4fcf0004355a905317c657d558f2d8ee2
Jake Lu <jylu1205@outlook.com>
2024-09-27 15:31:12 +0800

    fixed autograd

--- coeus/coeus_models/autograd.py
+++ coeus/coeus_models/autograd.py
@@ -1,61 +1,21 @@
 import numpy as np
 import cupy as cp
 
-# class autograd:
-#     def __init__(self, data, parents=None, op=None):
-#         self.data = data
-#         self.grad = 0.0
-#         self.parents = parents or []
-#         self.op = op
-    
-#     def backward(self, grad=1.0):
-#         self.grad += grad
-#         for parent, local_grad in self.parents:
-#             parent.backward(grad * local_grad(self))
-
-#     def __add__(self, other):
-#         other = other if isinstance(other, autograd) else autograd(other)
-#         return autograd(
-#             self.data + other.data,
-#             parents=[(self, lambda _: 1), (other, lambda _: 1)], op='+')
-
-#     def __mul__(self, other):
-#         other = other if isinstance(other, autograd) else autograd(other)
-#         return autograd(
-#             self.data * other.data,
-#             parents=[
-#                 (self, lambda v: other.data),
-#                 (other, lambda v: self.data)],
-#             op='*')
-
-#     def __sub__(self, other):
-#         other = other if isinstance(other, autograd) else autograd(other)
-#         return autograd(
-#             self.data - other.data,
-#             parents=[(self, lambda _: 1), (other, lambda _: -1)], op='-')
-
-#     def __truediv__(self, other):
-#         other = other if isinstance(other, autograd) else autograd(other)
-#         return autograd(
-#             self.data / other.data,
-#             parents=[
-#                 (self, lambda v: 1/other.data),
-#                 (other, lambda v: -self.data/other.data**2)],
-#             op='/')
-
-#     def __repr__(self):
-#         return f"autograd(data={self.data}, grad={self.grad})"
-
 class autograd:
-    def __init__(self, data, parents=None, op=None, device = "cpu"):
+    def __init__(self, data, parents=None, op=None, device="cpu"):
         if device == "cpu":
-            self.device = np
-        
-        if device == "gpu":
-            self.device = cp
+            self.device = "cpu"
+            self.module = np
+            self.data = np.array(data)
+            self.grad = np.zeros(self.data.shape)
+        elif device == "gpu":
+            self.device = "gpu"
+            self.module = cp
+            self.data = cp.array(data)
+            self.grad = cp.zeros(self.data.shape)
+        else:
+            raise ValueError("Unsupported device. Use 'cpu' or 'gpu'.")
 
-        self.data = self.device.array(data)
-        self.grad = self.device.zeros(self.data.shape)
         self.parents = parents or []
         self.op = op
     
@@ -71,14 +31,14 @@ class autograd:
         return autograd(
             self.data.transpose(),
             parents=[(self, lambda v: v.transpose())],
-            op='transpose')
-
+            op='transpose', device=self.device
+        )
     
     def __add__(self, other):
         other = other if isinstance(other, autograd) else autograd(other)
         return autograd(
             self.data + other.data,
-            parents=[(self, lambda _: 1), (other, lambda _: 1)], op='+')
+            parents=[(self, lambda _: 1), (other, lambda _: 1)], op='+', device=self.device)
     
     def __mul__(self, other):
         other = other if isinstance(other, autograd) else autograd(other)
@@ -87,16 +47,18 @@ class autograd:
             parents=[
                 (self, lambda v: other.data),
                 (other, lambda v: self.data)],
-            op='*')
+            op='*', device=self.device)
     
     def __matmul__(self, other):
         other = other if isinstance(other, autograd) else autograd(other)
         return autograd(
             self.data @ other.data,
             parents=[
-                (self, lambda v: other.data),
-                (other, lambda v: self.data)],
-            op='@')
+                (self, lambda v: other.data.T),
+                (other, lambda v: self.data.T)
+            ],
+            op='@', device=self.device
+        )
     
     def __pow__(self, other):
         other = other if isinstance(other, autograd) else autograd(other)
@@ -105,16 +67,16 @@ class autograd:
             parents=[
                 (self, lambda v: other.data * self.data**(other.data-1)),
                 (other, lambda v: self.data**other.data * np.log(self.data))],
-            op='**')
-
+            op='**', device=self.device)
+    
     def __neg__(self):
-        return autograd(-self.data, parents=[(self, lambda _: -1)], op='-')
+        return autograd(-self.data, parents=[(self, lambda _: -1)], op='-', device=self.device)
     
     def __sub__(self, other):
         other = other if isinstance(other, autograd) else autograd(other)
         return autograd(
             self.data - other.data,
-            parents=[(self, lambda _: 1), (other, lambda _: -1)], op='-')
+            parents=[(self, lambda _: 1), (other, lambda _: -1)], op='-', device=self.device)
     
     def __truediv__(self, other):
         other = other if isinstance(other, autograd) else autograd(other)
@@ -123,17 +85,14 @@ class autograd:
             parents=[
                 (self, lambda v: 1/other.data),
                 (other, lambda v: -self.data/other.data**2)],
-            op='/')
-    
-    
+            op='/', device=self.device)
 
     def __repr__(self):
         return f"autograd(data={self.data}, grad={self.grad})"
-    
+
 if __name__ == "__main__":
-    x = autograd(np.array([[1,2,3],[4,5,6]]))
-    y = autograd(np.array([[7,8,9],[10,11,12]]))
-    z = x @ y.transpose()
+    x = autograd([1,2,3], device="cpu")
+    y = autograd([0.5,0.5,0.5], device="gpu")
+    z = x ** y
     z.backward()
     print(x.grad)
-    print(y.grad)
